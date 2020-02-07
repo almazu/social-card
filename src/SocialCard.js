9619cbd75e82361d5a839e2bf2e7e85634efc1e1
almazu <almazuleyma@gmail.com>
2020-02-07 15:43:01 -0700

    Created new file, edited App.js to return Card

---
+++ src/SocialCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const Card = () => {
+    
+    return (
+        <div>
+            <p>Hello world.</p>
+        </div>
+    );
+}
+
+
+export default Card;
+
+// ReactDOM.Render(<Card />, document.getElementById("root"))
+
+// example of a functional component //
+// const Test = () => {
+    
+//     function testing(){
+//         alert('Test Output');
+//     }
+
+//     return (
+//         <button onClick={testing}>Click to test.</button>
+//     );
+// }
+
+// export default Test;
