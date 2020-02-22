ee53596f27d0da053d57417e21340f5fbf7006f1
almazu <almazuleyma@gmail.com>
2020-02-21 22:33:48 -0700

    Created class components and added styles

--- src/SocialCard.js
+++ src/SocialCard.js
@@ -1,30 +1,66 @@
 import React from 'react';
+import './styles.css';
 import ReactDOM from 'react-dom'
 
-const Card = () => {
-    
-    return (
-        <div>
-            <p>Hello world.</p>
-        </div>
-    );
+class Card extends React.Component {  
+    render() {                
+        return (
+            <div className='card'>
+                <User />
+                <Post />
+                <Stats />
+                <Share />
+            </div>
+        );
+    }
 }
 
+class User extends React.Component {
+    render() {
+        var username = "@ handle username"        
+        var logoImage = require('./logopt1.jpg')
+        return (   
+            <div className='user-account'>
+                <img src={logoImage} width={60} height={45}/>
+                {username}
+            </div>
+        );
+    }
+    
+}
+
+class Post extends React.Component {
+    render() {
+        var post = "Post Content"
+        var postImage = require('./cube.png')
+        return (
+            <div className='post-content'>
+                {post}
+                <img src={postImage} width={580} height={350} alt="Content Img"></img> 
+            </div>
+        )
+    }
+}
+
+class Stats extends React.Component {
+    render() {
+        var stats = "Retweets ... Likes"
+        return (
+            <div className='stats'>{stats}</div>
+        );
+    }
+}
+
+class Share extends React.Component {
+    render() {
+        return (
+            <div className='share-buttons'>
+                <a href="#" className='like-button'>Like</a>
+                <a href="#" className='comment-button'>Comment</a>
+                <a href="#" className='share-button'>Share</a>
+            </div>
+        );
+    }
+}
 
 export default Card;
-
-// ReactDOM.Render(<Card />, document.getElementById("root"))
-
-// example of a functional component //
-// const Test = () => {
-    
-//     function testing(){
-//         alert('Test Output');
-//     }
-
-//     return (
-//         <button onClick={testing}>Click to test.</button>
-//     );
-// }
-
-// export default Test;
